ae8b5ea135246a5542d5543e51ec181c856ef404
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2024-10-28 09:11:17 +0100

    add extension flag

--- FireBrowserWinUi3/App.xaml.cs
+++ FireBrowserWinUi3/App.xaml.cs
@@ -70,13 +70,11 @@ public partial class App : Application
         _ = FireBrowserWinUi3Navigator.TLD.LoadKnownDomainsAsync().ConfigureAwait(false);
 
         Environment.SetEnvironmentVariable("WEBVIEW2_USE_VISUAL_HOSTING_FOR_OWNED_WINDOWS", "1");
-        // to avoid flashing webview on desktop move to lower bottom right, hopefully off screen 
-        // ms says in webview runtime in mid oct /nov
 
         Environment.SetEnvironmentVariable("WEBVIEW2_CHANNEL_SEARCH_KIND", "1");
         Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--window-size=0,0 --window-position=40000,40000");
+        Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--enable-extensions");
         Windows.Storage.ApplicationData.Current.LocalSettings.Values["AzureStorageConnectionString"] = AzureStorage;
-
     }
 
     private void Current_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
