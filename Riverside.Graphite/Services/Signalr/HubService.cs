6168d9faea5671b2e65af33539ee71384a04764b
john doyle <fizzledbydizzle@live.com>
2024-12-01 21:17:30 -0500

    working on a hub service.

--- Riverside.Graphite/Services/Signalr/HubService.cs
+++ Riverside.Graphite/Services/Signalr/HubService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Riverside.Graphite.Runtime.Helpers.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,35 @@ using System.Threading.Tasks;
 
 namespace Riverside.Graphite.Services.Signalr
 {
-	internal class HubService
+	public class HubService
 	{
+		public HubService() {
+
+			Initialize(); 
+		}
+		private HubConnection _hubConnection;
+		private async void Initialize()
+		{
+			try
+			{
+				_hubConnection = new HubConnectionBuilder()
+					.WithUrl(@"https://energy.service.signalr.net/azurechat")
+					.WithAutomaticReconnect()
+					.ConfigureLogging(logging => logging.AddConsole())
+					.Build();
+
+				_hubConnection.Closed += async (error) => await _hubConnection.StartAsync();
+				_hubConnection.On<string>("sendNotify", async (message) => ExceptionLogger.LogInformation($"{message}\r\n"));
+				await _hubConnection.StartAsync();
+				Console.WriteLine("Listening for a message"); 
+			}
+			catch (Exception ex)
+			{
+				ExceptionLogger.LogException(ex);	
+			}
+		}
+			//(https://github.com/microsoft/WindowsAppSDK/discussions/3561)
+
+
 	}
 }
