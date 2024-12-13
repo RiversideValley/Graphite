37e1120c126e86a73649338c14878d61c1f994e9
john doyle <fizzledbydizzle@live.com>
2024-12-12 22:01:00 -0500

    syntax error corrected

--- Riverside.Graphite.Channels/Program.cs
+++ Riverside.Graphite.Channels/Program.cs
@@ -29,7 +29,7 @@ namespace Riverside.Graphite.Channels
 			
 		}
 
-		public static void Startup(WebApplicationBuilder builder) {`
+		public static void Startup(WebApplicationBuilder builder) {
 			
 			_ = builder.Services.AddHttpClient();
 			_ = builder.Services.AddRouting();
@@ -105,55 +105,6 @@ namespace Riverside.Graphite.Channels
 			app.Run();
 
 		}
-		//public static IHostBuilder CreateHostBuilder(string[] args)
-		//{
-		
-
-		//	return Host.CreateDefaultBuilder(args)
-		//		.ConfigureServices((hostContext, services) =>
-		//		{
-		//			_ = services.AddHttpClient();
-		//			_ = services.AddControllers(); 
-		//			_ = services.AddRouting();
-		//			_ = services.AddDistributedMemoryCache();
-		//			_ = services.AddSignalR(options =>
-		//			{
-		//				options.ClientTimeoutInterval = TimeSpan.FromMilliseconds(120000);
-		//				options.KeepAliveInterval = TimeSpan.FromMilliseconds(840000);
-		//			}).AddAzureSignalR(options =>
-		//			{
-		//				//  This is a tircky way to associate user name with connection for sample purpose.
-		//				//  For PROD, we suggest to use authentication and authorization, see here:
-		//				//  https://docs.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-2.2
-		//				options.ClaimsProvider = context =>
-		//				[
-		//					 new Claim(ClaimTypes.NameIdentifier, context?.Request?.Query["username"]!)
-		//				];
-		//				options.ServerStickyMode = Microsoft.Azure.SignalR.ServerStickyMode.Preferred;
-
-		//			});
-
-		//			_ = services.AddSingleton<IMessageHandler, AzureDataTableMessages>();
-		//			_ = services.AddSingleton<IAzureDataTableSessionStorage, AzureDataTableSession>();
-		//			_ = services.AddSingleton<SignalRService>()
-		//						.AddHostedService(sp => sp.GetService<SignalRService>())
-		//						.AddSingleton<IHubContextStore>(sp => sp.GetService<SignalRService>());
-		//			//					_= services.AddSingleton<SessionContext>();
-		//			_ = services.AddSingleton<IHubCommander, HubCommander>();
-				   
-		//			_= services.AddSingleton<AzureChat>();
-		//			//_ = services.AddHostedService<Worker>(); 
-		//			//_ = services.AddWindowsService(op =>
-		//			//{
-		//			//	op.ServiceName = "GraphiteChannel";
-		//			//});
-					
-		//			_ = services.AddLogging(configure => configure.AddConsole())
-		//						.Configure<LoggerFilterOptions>(options => options.MinLevel = Microsoft.Extensions.Logging.LogLevel.Information);
-		//		}).ConfigureLogging(options =>
-		//		{
-		//			_ = options.AddEventSourceLogger();
-		//		});
-		//}
+	
 	}
 }
