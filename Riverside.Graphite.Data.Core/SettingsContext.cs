fbee92886b46cd7c3d3e3fc97a3d136fd87bbe16
john doyle <fizzledbydizzle@live.com>
2025-01-19 02:58:26 -0500

    finally migration.  sql in schema.

--- Riverside.Graphite.Data.Core/SettingsContext.cs
+++ Riverside.Graphite.Data.Core/SettingsContext.cs
@@ -13,7 +13,7 @@ public class SettingsContext : DbContext
 {
 	public DbSet<Settings> Settings { get; set; }
 	public string ConnectionPath { get; set; }
-	public SettingsContext(string username = "dizzler")
+	public SettingsContext(string username)
 	{
 		ConnectionPath = username == null
 			? throw new ArgumentNullException(nameof(username))
