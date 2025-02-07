acf1d4268047bf3505838d8a5e22093285db61c3
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2025-02-07 12:14:34 +0100

    fix2

--- Graphite/MainWindow.xaml.cs
+++ Graphite/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ public sealed partial class MainWindow : Window
 	public MainWindow()
 	{
 		this.InitializeComponent();
+		_windowHandler.Initialize(this);
 		_windowHandler.SetTitle("Graphite Login");
 		_windowHandler.SetIcon("Logo.ico");
 		_windowHandler.SetWindowBackdrop(BackdropType.MicaAlt);
