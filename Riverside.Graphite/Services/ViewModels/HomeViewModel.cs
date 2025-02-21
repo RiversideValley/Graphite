638a6892b6d4b4ac014d5e54e9ee01581164974d
john doyle <fizzledbydizzle@live.com>
2025-02-21 03:55:10 -0500

    minor bug fix.

--- Riverside.Graphite/Services/ViewModels/HomeViewModel.cs
+++ Riverside.Graphite/Services/ViewModels/HomeViewModel.cs
@@ -401,7 +401,7 @@ public partial class HomeViewModel : ObservableRecipient
 					switch (btn.Tag.ToString())
 					{
 						case "Settings":
-							window.Tabs.TabItems.Add(window.TabManager.CreateNewTab(typeof(SettingsPage)));
+							window.TabManager.CreateNewTab(typeof(SettingsPage));
 							window.SelectNewTab();
 							break;
 						case "Downloads":
