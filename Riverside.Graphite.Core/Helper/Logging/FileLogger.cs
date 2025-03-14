46a0de9d0e726579c9c5b0ca5d884cb5fc3e168a
john doyle <fizzledbydizzle@live.com>
2025-03-14 04:05:11 -0400

    mandate and delte file older than a day

--- Riverside.Graphite.Core/Helper/Logging/FileLogger.cs
+++ Riverside.Graphite.Core/Helper/Logging/FileLogger.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 namespace Riverside.Graphite.Core.Helper.Logging
@@ -14,6 +16,15 @@ namespace Riverside.Graphite.Core.Helper.Logging
             string logDirectory = Path.Combine(localAppData);
             Directory.CreateDirectory(logDirectory);
             _filePath = Path.Combine(logDirectory, $"MigrationLog_{DateTime.Now:yyyyMMddHHmmss}.txt");
+
+            // Delete files older than a day
+            var directoryInfo = new DirectoryInfo(logDirectory);
+            var oldFiles = directoryInfo.GetFiles()
+                                        .Where(file => file.CreationTime < DateTime.Now.AddDays(-1));
+            foreach (var file in oldFiles)
+            {
+                file.Delete();
+            }
         }
 
         public IDisposable BeginScope<TState>(TState state) => null;
@@ -27,26 +38,37 @@ namespace Riverside.Graphite.Core.Helper.Logging
         }
     }
 
-	public class TabMangerLogger<T> : ILogger<T>
-	{
-		private readonly string _filePath;
+    public class TabMangerLogger<T> : ILogger<T>
+    {
+        private readonly string _filePath;
 
-		public TabMangerLogger()
-		{
-			string localAppData = UserManager.GraphiteDataPath; //  Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-			string logDirectory = Path.Combine(localAppData, AuthService.CurrentUser?.Username, "Logs");
-			Directory.CreateDirectory(logDirectory);
-			_filePath = Path.Combine(logDirectory, $"TabStates_{DateTime.Now:yyyyMMdd}.txt");
-		}
+        public TabMangerLogger()
+        {
+            string localAppData = UserManager.GraphiteDataPath;
+            string logDirectory = Path.Combine(localAppData, AuthService.CurrentUser?.Username, "Logs");
+            Directory.CreateDirectory(logDirectory);
+            _filePath = Path.Combine(logDirectory, $"TabStates_{DateTime.Now:yyyyMMdd}.txt");
 
-		public IDisposable BeginScope<TState>(TState state) => null;
+            // Delete files older than a day
+			Task.Factory.StartNew(() => {
+				var directoryInfo = new DirectoryInfo(logDirectory);
+				var oldFiles = directoryInfo.GetFiles()
+											.Where(file => file.CreationTime < DateTime.Now.AddDays(-1));
+				foreach (var file in oldFiles)
+				{
+					file.Delete();
+				}
+			});
+        }
 
-		public bool IsEnabled(LogLevel logLevel) => true;
+        public IDisposable BeginScope<TState>(TState state) => null;
 
-		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-		{
-			string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{logLevel}] {formatter(state, exception)}";
-			File.WriteAllText(_filePath, logMessage + Environment.NewLine);
-		}
-	}
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{logLevel}] {formatter(state, exception)}";
+            File.WriteAllText(_filePath, logMessage + Environment.NewLine);
+        }
+    }
 }
