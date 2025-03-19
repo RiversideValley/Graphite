94644a4c7836211aa698e92803c2e9d9e64f559d
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2025-03-19 14:40:49 +0100

    bring back minor fix from johndizzle

--- Riverside.Graphite/Pages/WebContent.xaml.cs
+++ Riverside.Graphite/Pages/WebContent.xaml.cs
@@ -280,7 +280,7 @@ namespace Riverside.Graphite.Pages
 			s.CoreWebView2.WebResourceResponseReceived += WebResourceResponseReceived;
 			s.CoreWebView2.PermissionRequested += PermissionRequested;
 		
-			AdBlockerService.Toggle(true);
+			AdBlockerService.Toggle(false);
 			await AdBlockerService.Initialize(WebView);
 		}
 
